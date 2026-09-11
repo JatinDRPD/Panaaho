4d103cc13063dec2b62e26f293fd3d517eaae506
Jatin Dwivedi <jatin.drpd@gmail.com>
2026-09-11 10:16:02 +0530

    Renamed

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname,"/publics")));
 app.engine("ejs", ejsMate);
 
 main().then(() => {
